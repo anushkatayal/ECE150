3f74267f2cb2d6cf0b23efedb600e0915614fac4
Anushka Tayal <anushkatayal@Anushkas-MacBook-Pro.local>
2018-09-05 19:09:39 +0530

    Lab 0.3

---
+++ Me.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std; //library for input output files standard
+
+int main() //starting point ofprogram
+{
+	cout<< "My name is Anushka Tayal. " << endl; //name of person
+	cout<< "My UserID is atayal." << endl; //userID
+	cout<< "I like programming because it is logical." << endl; //endl to have a new line
+	return 0; 
+}
