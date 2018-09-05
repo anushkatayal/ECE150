1041cf9d70a2b3b6004af642f063b011f6a9dc87
Anushka Tayal <anushkatayal@Anushkas-MacBook-Pro.local>
2018-09-05 18:59:48 +0530

    Lab 0.2

---
+++ Welcome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main( const int argc, const char* const argv[])
+{
+    cout << "Welcome to UW Engineering, " << argv[1] << "." << endl;
+
+    cout << "Your University of Waterloo E-mail address is: " << argv[2] << "@uwaterloo.ca" << endl;
+     // add a line of code to output the second command-line argument (argv[2])
+
+    cout << "The value stored in argc is " << argc << "." << endl; // add a line of code to output the content of argc	
+ 
+
+    cout << "The variable \"argc\" contains the length of argv." << endl; // add a line of code to explain what is stored in argc
+
+
+    return 0;
+}
