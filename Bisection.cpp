918ff314e5dbc14c463870a7ffbb5893e7820b9a
Anushka Tayal <tayal1anushka@gmail.com>
2018-10-02 23:48:20 +0530

    Lab 5.1

---
+++ Bisection.cpp
@@ -0,0 +1,77 @@
+//////////////////////////////////////////////////////////////
+//
+// Your #includes here; make sure you are allowed them ...
+//
+
+#include <stdlib.h>
+
+//////////////////////////////////////////////////////////////
+//
+// #includes and function declarations; do not remove
+//
+
+#ifndef MARMOSET_TESTING
+#include <iostream>
+using namespace std;
+
+
+// Sample function for which you need to find the root
+
+float f(const float x) {
+  return 3.2*x*x*x - 2.1*x - 23.0;
+}
+
+#endif
+
+//////////////////////////////////////////////////////////////
+//
+// Function declarations; do not remove
+// Replace "..." in bisectionHelper() with any parameter(s)
+// you need to ensure termination when precision and
+// minIntervalSize are too tightly specified.
+//
+
+float bisectionHelper(const float left, const float right,
+		      const float precision, const float minIntervalSize
+		      ... );
+float bisection(const float left, const float right,
+		const float precision, const float minIntervalSize);
+
+//////////////////////////////////////////////////////////////
+//
+// Your code here ...
+//  
+// Return std::numeric_limits<float>::quiet_NaN() (comes from
+// <limits>) if cannot compute the root for some reason
+//
+
+float bisectionHelper(const float left, const float right,
+		      const float precision, const float minIntervalSize
+		      ... );
+
+float bisection(const float left, const float right,
+		const float precision, const float minIntervalSize) {
+
+}
+
+//////////////////////////////////////////////////////////////
+//
+// Test Driver
+//
+// Do not remove the #ifndef and its associated #endif
+// This is required so that when you submit your code,
+// this test driver will be ignored by Marmoset
+//
+
+#ifndef MARMOSET_TESTING
+
+#define isNaN(X) (X != X)  // NaN is the only float that is not equal to itself
+
+int main(const int argc, const char* const argv[]) {
+  
+  // Some test driver code here ....
+
+  return 0;
+}
+
+#endif
