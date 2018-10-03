41f76c9b4e56fb48a2135db34b729bf2b1e42fb8
Anushka Tayal <tayal1anushka@gmail.com>
2018-10-03 23:45:09 +0530

    Week4Pt1

---
+++ string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  const int N=4;
+  char myString[N];
+
+  myString[0]='a';
+  myString[1]='b';
+  myString[2]='c';
+  // myString[3]='\0';
+
+  cout<<myString;
+}
