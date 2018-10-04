795ce583b698563e2ae930b3f5dbf7efcb1092d7
Anushka Tayal <tayal1anushka@gmail.com>
2018-10-04 23:45:56 +0530

    W4,S2

---
+++ string2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char textArray[5];
+    int varAfterArray;
+
+    cin >> textArray;
+    varAfterArray = 1;
+
+    cout << textArray;
+
+//   const int N=5;
+//   char textArray[N];
+//   int varAfterArray;
+
+//   char x = cin.get();
+//   int i = 0;
+
+// while (x != '\n' && i < N-1){
+//     textArray[i] = x;
+//     i++;
+//     x = cin.get();
+// }
+
+// textArray[i] = 0;
+}
